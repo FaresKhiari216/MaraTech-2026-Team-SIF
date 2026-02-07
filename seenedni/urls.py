dea3607501f887cb861f551a2cef33d0f2fe52b8
FaresKhiari261 <fares.khiari261@gmail.com>
2026-02-07 08:00:36 +0100

    adding login/register form + improving UI

--- seenedni/urls.py
+++ seenedni/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from UserApp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AnnouncementApp.urls')),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
 
 # Serve static files during development
